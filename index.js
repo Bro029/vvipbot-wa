8ff50f692fa73f61ae64ca3a1376f75f005f6eb3
Bro029 <nabilalfatih916@gmail.com>
2024-10-14 12:10:50 +0700

    Update index.js

--- index.js
+++ index.js
@@ -15,14 +15,14 @@ const menu = require("./lib/menu.js")
 
 const apivhtear = 'Apikey vhtear';
 const apibarbar = 'Apikey mhankbarbar';
-const BotName = 'VVIPINDO BOT'; 
-const instagram = 'agus_alnsyh71- kaelMk_'; 
-const aktif = 'Kapan bot aktif';
+const BotName = 'BR0BOT'; 
+const instagram = 'https://www.instagram.com/nab_ilbae?igsh=MTBwY3MzbTc1YWd0Yw=='; 
+const aktif = '01:00-00:00';
 const vcard = 'BEGIN:VCARD\n' // metadata of the contact card
             + 'VERSION:3.0\n' 
-            + 'FN:Agus\n' // Nama kamu
-            + 'ORG:VVIPINDO;\n' // Nama bot
-            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //Nomor whatsapp kamu
+            + 'FN:Agus\n' //Bro.cuy
+            + 'ORG:VVIPINDO;\n' // BR0BOT
+            + 'TEL;type=CELL;type=VOICE;waid=6289613469459:+62 896-1346-9459\n' //6288983298862
             + 'END:VCARD'
 const
 {
